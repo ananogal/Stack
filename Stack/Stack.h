72bee0dcf461664cb74fc8332cbb77f9124f91f0
Ana Nogal <ana.nogal@gmail.com>
2015-07-28 15:10:17 +0100

    Added Xcode project

---
+++ Stack/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  Stack
+//
+//  Created by Ana Nogal on 28/07/2015.
+//  Copyright (c) 2015 Ana Nogal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+@end
